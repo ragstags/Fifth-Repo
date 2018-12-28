df6594fc7e1206918013b1595f9eeeec141e087b
ragstags <shazia.alphaauth1@gmail.com>
2018-12-28 19:14:04 +0000

    commit 1

---
+++ src/com/Practice/Hello.java
@@ -0,0 +1,10 @@
+package com.Practice;
+
+/**
+ * Created by nadimchouglay on 28/12/2018.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
